7b3688de25febe63428ccfccbc697303c202909b
ninjamateifort <65023740+ninjamateifort@users.noreply.github.com>
2021-08-20 12:20:15 +0300

    Just compacted the code
    So you should know that the loop you put is useless and this is better

--- RickRoll.cpp
+++ RickRoll.cpp
@@ -2,20 +2,7 @@
 #include <windows.h>
 #include <shellapi.h>
 
- using namespace std; 
-
  int main()
  {
-     for (int dm = 0; dm < 1; dm++)
-     {
-         ShellExecute(0,0, "https://www.youtube.com/watch?v=dQw4w9WgXcQ", 0, 0, SW_HIDE);
-
-     }
-     return 0;
-
-
-
+     ShellExecute(0, 0, L"https://www.youtube.com/watch?v=dQw4w9WgXcQ", 0, 0, SW_HIDE);
  }
-
-
- 
